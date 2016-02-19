c388964156d9eb647d67748ae4376e1413defb07
lexvalyaev <lexvalyaev@gmail.com>
2016-02-19 16:00:42 +0500

    Add sql scripts

---
+++ test_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO test.user (id, name, age, isAdmin, createdDate) VALUES (1, 'one', 22, false, '2016-02-15 21:54:27');
+INSERT INTO test.user (id, name, age, isAdmin, createdDate) VALUES (2, 'two', 33, false, '2016-02-15 21:55:31');
+INSERT INTO test.user (id, name, age, isAdmin, createdDate) VALUES (3, 'asdff', 23, false, '2016-02-19 13:16:37');
+INSERT INTO test.user (id, name, age, isAdmin, createdDate) VALUES (4, 'qweqwe', 334, false, '2016-02-19 13:36:23');
+INSERT INTO test.user (id, name, age, isAdmin, createdDate) VALUES (5, 'fff', 555, false, '2016-02-19 14:16:40');
